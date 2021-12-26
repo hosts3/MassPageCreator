d7def3942d2c6ace66e29867d94e661d4515e979
jgriggs@mailfence.com <jgriggs@mailfence.com>
2021-12-26 02:53:54 +0000

    File generated by Skaffolder - user: jgriggs@mailfence.com

---
+++ api/properties.php
@@ -0,0 +1,10 @@
+<?php 
+	$db_MassPageCreator_db_Url = $_ENV["DB_HOST"] ?? '127.0.0.1';
+	$db_MassPageCreator_db_User = 'root';
+	$db_MassPageCreator_db_Pass = '';
+	$db_MassPageCreator_db_DbName = 'masspagecreator_db';
+	
+	
+	$jwt_secret_key = 'mySecretKey';
+	
+?>
